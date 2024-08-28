0f069579ab21bfd20daacc03a409ace42ff70f9c
aboufofana <aboufofana76@gmail.com>
2024-08-28 23:01:07 +0200

    Add testcontainers dependency for rabbitmq and configure it

--- src/test/java/com/sinignaci/orderservice/TestcontainersConfiguration.java
+++ src/test/java/com/sinignaci/orderservice/TestcontainersConfiguration.java
@@ -3,6 +3,7 @@ package com.sinignaci.orderservice;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -13,12 +14,24 @@ class TestcontainersConfiguration {
 	@Container
 	static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
 
+	public static final int RABBITMQ_PORT = 5672;
+	@Container
+	static RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management"))
+			.withExposedPorts(RABBITMQ_PORT);
+
+
 	@DynamicPropertySource
-	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+	static void dynamicProperties(DynamicPropertyRegistry registry) {
+
 		registry.add("spring.r2dbc.url", TestcontainersConfiguration::r2dbcUrl);
 		registry.add("spring.r2dbc.username", postgresql::getUsername);
 		registry.add("spring.r2dbc.password", postgresql::getPassword);
 		registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+
+		registry.add("spring.rabbitmq.host", rabbitmq::getHost);
+		registry.add("spring.rabbitmq.port", () -> RABBITMQ_PORT);
+		registry.add("spring.rabbitmq.username", rabbitmq::getAdminUsername);
+		registry.add("spring.rabbitmq.password", rabbitmq::getAdminPassword);
 	}
 
 	private static String r2dbcUrl() {
