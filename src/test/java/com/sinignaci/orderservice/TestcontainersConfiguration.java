f0f1f5b578fece4cb01d5a8fc59db8a6a679b784
aboufofana <aboufofana76@gmail.com>
2024-08-28 23:09:47 +0200

    fix rabbitmq test configuration

--- src/test/java/com/sinignaci/orderservice/TestcontainersConfiguration.java
+++ src/test/java/com/sinignaci/orderservice/TestcontainersConfiguration.java
@@ -11,31 +11,29 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 class TestcontainersConfiguration {
 
-	@Container
-	static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+    @Container
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
 
-	public static final int RABBITMQ_PORT = 5672;
-	@Container
-	static RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management"))
-			.withExposedPorts(RABBITMQ_PORT);
+    @Container
+    static RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3"));
 
 
-	@DynamicPropertySource
-	static void dynamicProperties(DynamicPropertyRegistry registry) {
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
 
-		registry.add("spring.r2dbc.url", TestcontainersConfiguration::r2dbcUrl);
-		registry.add("spring.r2dbc.username", postgresql::getUsername);
-		registry.add("spring.r2dbc.password", postgresql::getPassword);
-		registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+        registry.add("spring.r2dbc.url", TestcontainersConfiguration::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
 
-		registry.add("spring.rabbitmq.host", rabbitmq::getHost);
-		registry.add("spring.rabbitmq.port", () -> RABBITMQ_PORT);
-		registry.add("spring.rabbitmq.username", rabbitmq::getAdminUsername);
-		registry.add("spring.rabbitmq.password", rabbitmq::getAdminPassword);
-	}
+        registry.add("spring.rabbitmq.host", rabbitmq::getHost);
+        registry.add("spring.rabbitmq.port", rabbitmq::getAmqpPort);
+        registry.add("spring.rabbitmq.username", rabbitmq::getAdminUsername);
+        registry.add("spring.rabbitmq.password", rabbitmq::getAdminPassword);
+    }
 
-	private static String r2dbcUrl() {
-		return String.format("r2dbc:postgresql://%s:%s/%s", postgresql.getHost(),
-				postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT), postgresql.getDatabaseName());
-	}
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s", postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT), postgresql.getDatabaseName());
+    }
 }
